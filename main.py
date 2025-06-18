5be44635cd687a49635808e9e789993784c74940
RonaldLiao <graduallymomsoup@gmail.com>
2025-06-18 19:37:58 +0800

    更新網站註解內容，提供更詳細的使用說明

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ import streamlit as st
 st.title("這是首頁")
 
 """
-註解
-"""
+如何更新網站：
 
+一、點選畫面右上角三角形 ▶
+
+1.先自動存檔
+
+2.Black Formatter 幫你排版
+
+3.執行程式
+
+二、git上傳步驟
+分享copilot進行提交與推送
+
+三、套用模組
+開啟終端機輸入:streamlit run main.py
+
+"""
 
 print("Hello")
